f09ca4ce4ef3a4ddc75d442f2357efc2e16281f3
Joselito <Joselito@DESKTOP-K8JI8L3>
2025-07-13 19:46:22 -0600

    Avance Semana 9

--- src/main/java/com/tienda/controller/PruebasController.java
+++ src/main/java/com/tienda/controller/PruebasController.java
@@ -5,6 +5,7 @@ import com.tienda.domain.Producto;
 import com.tienda.service.CategoriaService;
 import com.tienda.service.ProductoService;
 import com.tienda.service.impl.FirebaseStorageServiceImpl;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,22 +18,22 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 @RequestMapping("/pruebas")
 public class PruebasController {
-    
-@Autowired
-private ProductoService productoService;
 
-@Autowired
-private CategoriaService categoriaService;
+    @Autowired
+    private ProductoService productoService;
 
-@GetMapping("/listado")
-public String listado(Model model) {
-    var lista = productoService.getProductos(false);
-    var categorias = categoriaService.getCategorias(true);
-    model.addAttribute("productos", lista);
-    model.addAttribute("totalProductos", lista.size());
-    model.addAttribute("categorias", categorias);
-    return "/pruebas/listado";
-    
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping("/listado")
+    public String listado(Model model) {
+        var lista = productoService.getProductos(false);
+        var categorias = categoriaService.getCategorias(true);
+        model.addAttribute("productos", lista);
+        model.addAttribute("totalProductos", lista.size());
+        model.addAttribute("categorias", categorias);
+        return "/pruebas/listado";
+
     }
 
     @GetMapping("/listado/{idCategoria}")
@@ -44,6 +45,83 @@ public String listado(Model model) {
         model.addAttribute("categorias", categorias);
         return "/pruebas/listado";
     }
+
+    //Los métodos siguientes son para la prueba de consultas ampliadas
+    @GetMapping("/listado2")
+    public String listado2(Model model) {
+        var productos = productoService.getProductos(false);
+        model.addAttribute("productos", productos);
+        return "/pruebas/listado2";
+    }
+
+    @PostMapping("/query1")
+    public String consultaQuery1(@RequestParam(value = "precioInf") double precioInf,
+            @RequestParam(value = "precioSup") double precioSup,
+            Model model) {
+        var productos = productoService.findByPrecioBetweenOrderByDescripcion(precioInf, precioSup);
+        model.addAttribute("productos", productos);
+        model.addAttribute("totalProductos", productos.size());
+        model.addAttribute("precioInf", precioInf);
+        model.addAttribute("precioSup", precioSup);
+        return "/pruebas/listado2";
+    }
+
+    @PostMapping("/query2")
+    public String consultaQuery2(@RequestParam(value = "precioInf") double precioInf,
+            @RequestParam(value = "precioSup") double precioSup,
+            Model model) {
+        var productos = productoService.metodoJPQL(precioInf, precioSup);
+        model.addAttribute("productos", productos);
+        model.addAttribute("totalProductos", productos.size());
+        model.addAttribute("precioInf", precioInf);
+        model.addAttribute("precioSup", precioSup);
+        return "/pruebas/listado2";
+    }
+    
+    @GetMapping("/listado3")
+    public String listado3(Model model) {
+        var categorias = categoriaService.getCategorias(false); 
+        model.addAttribute("categorias", categorias);
+        return "/pruebas/listado3";
+    }
+
+    @PostMapping("/queryCategoria")
+    public String queryCategoria(@RequestParam("descripcion") String descripcion, Model model) {
+        List<Categoria> categorias;
+
+        if (descripcion == null || descripcion.trim().isEmpty()) {
+            categorias = categoriaService.getCategorias(false); 
+        } else {
+            categorias = categoriaService.filtrarPorNombre(descripcion);
+        }
+
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("descripcion", descripcion); 
+        return "/pruebas/listado3";
+    }
+
+    @PostMapping("/filtrarEstado")
+    public String filtrarEstadoCategoria(@RequestParam("estado") String estado, Model model) {
+        List<Categoria> categorias;
+
+        switch (estado) {
+            case "activas" ->
+                categorias = categoriaService.getCategorias(true); 
+            case "inactivas" -> {
+                categorias = categoriaService.getCategorias(false);
+                categorias.removeIf(Categoria::isActivo); 
+            }
+            default ->
+                categorias = categoriaService.getCategorias(false); 
+        }
+
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("estado", estado);
+        return "/pruebas/listado3";
+    }
+
+
+
 }
 
 
